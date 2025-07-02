493f4f79f1acf0f56dad526bcf55520749f15a96
Adetuyi Tolu Emmanuel <tolu.adetuyi@gmail.com>
2025-07-02 16:21:01 -0400

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -5,7 +5,8 @@ GROUP BY user_id;
 SELECT 
     property_id,
     COUNT(*) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS rank
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank,
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_row_number
 FROM bookings
 GROUP BY property_id;
 
